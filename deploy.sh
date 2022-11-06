362a8705ef973f9224ca2322865bda1104ade396
AqingCyan <aqingcyan@gmail.com>
2022-11-07 04:56:57 +0800

    feat: try deploy

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-docker stop ta_dah
-docker rm ta_dah
-docker rmi ccr.ccs.tencentyun.com/ta_dah/ta_dah
-docker pull jpccr.ccs.tencentyun.com/ta_dah/ta_dah
-docker run -d --name ta_dah -p 80:3000 jpccr.ccs.tencentyun.com/ta_dah/ta_dah

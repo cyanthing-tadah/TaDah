88c61e528d37c5761d3fee26fc44a487687fa0b9
AqingCyan <aqingcyan@gmail.com>
2022-11-07 04:29:31 +0800

    feat: try deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #/bin/bash
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
-docker rmi jpccr.ccs.tencentyun.com/ta_dah/ta_dah
+docker stop ta_dah
+docker rm ta_dah
+docker rmi ccr.ccs.tencentyun.com/ta_dah/ta_dah
 docker pull jpccr.ccs.tencentyun.com/ta_dah/ta_dah
-docker run -d --name 镜像名称 -p 80:3000 jpccr.ccs.tencentyun.com/ta_dah/ta_dah
+docker run -d --name ta_dah -p 80:3000 jpccr.ccs.tencentyun.com/ta_dah/ta_dah

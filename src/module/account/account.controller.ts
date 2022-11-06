7de307e68a7f9a90b83a0dc91a3b2f147a24fc60
AqingCyan <aqingcyan@gmail.com>
2022-11-07 04:00:38 +0800

    fix: interceptor and filter just use in account module

--- src/module/account/account.controller.ts
+++ src/module/account/account.controller.ts
@@ -1,6 +1,10 @@
-import { BadRequestException, Controller, Get } from '@nestjs/common'
+import { BadRequestException, Controller, Get, UseFilters, UseInterceptors } from '@nestjs/common'
+import { HttpExceptionFilter } from '../../core/filters/http-exception.filter'
+import { TransformResponseInterceptor } from '../../core/interceptors/transform-response.interceptor'
 
 @Controller('account')
+@UseInterceptors(TransformResponseInterceptor)
+@UseFilters(HttpExceptionFilter)
 export class AccountController {
   @Get('/success')
   testSuccess() {
--- src/module/account/account.module.ts
+++ src/module/account/account.module.ts
@@ -1,8 +1,5 @@
 import { Module } from '@nestjs/common'
-import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core'
 import { TypeOrmModule } from '@nestjs/typeorm'
-import { HttpExceptionFilter } from '../../core/filters/http-exception.filter'
-import { TransformResponseInterceptor } from '../../core/interceptors/transform-response.interceptor'
 import { AccountController } from './account.controller'
 import { AccountService } from './account.service'
 import { WexinUserAccountEntity } from './account.entity'
@@ -10,11 +7,7 @@ import { WexinUserAccountEntity } from './account.entity'
 @Module({
   imports: [TypeOrmModule.forFeature([WexinUserAccountEntity])],
   controllers: [AccountController],
-  providers: [
-    AccountService,
-    { provide: APP_INTERCEPTOR, useClass: TransformResponseInterceptor },
-    { provide: APP_FILTER, useClass: HttpExceptionFilter },
-  ],
+  providers: [AccountService],
   exports: [AccountService],
 })
 export class AccountModule {}

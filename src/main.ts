92959e9e57a36e77f8309195436bdedcad17c846
AqingCyan <aqingcyan@gmail.com>
2022-12-11 05:33:10 +0800

    feat: open env check

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
+import * as process from 'process'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 const PORT = 3000
 
+const baseOption = { credentials: true }
+const corsOption = process.env.NODE_ENV !== 'prod' ? baseOption : { ...baseOption, credentials: true }
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  app.enableCors({
-    origin: 'https://tally.cyanthing.com',
-    credentials: true,
-  })
+  app.enableCors(corsOption)
   await app.listen(PORT)
 }
 bootstrap().then(() => console.log(`Application is running on port: ${PORT} 🚀`))

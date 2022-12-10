6e39eaf513f4c9d33737aa8720b3109253c7281d
AqingCyan <aqingcyan@gmail.com>
2022-12-10 22:50:47 +0800

    feat: add helmet

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ const PORT = 3000
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  app.enableCors({ credentials: true })
+  app.enableCors({
+    origin: 'https://tally.cyanthing.com',
+    credentials: true,
+  })
   await app.listen(PORT)
 }
 bootstrap().then(() => console.log(`Application is running on port: ${PORT} 🚀`))
